bfcb478419e1201533a14a09a0893e999528af45
abel2319 <abelbakary@gmail.com>
2022-10-14 11:41:04 +0100

    task0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,7 @@ void bubble_sort(int *array, size_t size)
 	unsigned long int i, j;
 	int tmp = 0;
 
-	if ((array == NULL) || (size < 0))
+	if ((array == NULL) || (size < 1))
 		return;
 
 	for (i = 0; i < (size - 1); i++)
